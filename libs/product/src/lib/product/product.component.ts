675251475b4deb0bd61c27f8c07bfddc51bbf1db
saianushakandibanda <ksaianusha93@gmail.com>
2024-06-20 01:10:34 -0400

    add product lib

---
+++ libs/product/src/lib/product/product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css',
+})
+export class ProductComponent {}
